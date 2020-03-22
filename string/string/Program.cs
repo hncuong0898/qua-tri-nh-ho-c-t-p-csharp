2418262fab155c53667c3510bab841ab2eef1b32
NguyenCuong <honguyencuong0898@gmail.com>
2020-03-22 09:54:18 +0700

    bien string

---
+++ string/string/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String a = " haha ";       
+        string b = a.ToUpper();
+        string c = b.TrimStart();
+        string d = b.Substring(1,4);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.ReadKey();
+    }
+}
