eb31c3daf005dbf0b54ebb4bb83b431d12addeeb
NguyenCuong <honguyencuong0898@gmail.com>
2020-03-22 11:54:18 +0700

    baitapifelse

---
+++ baitapIfElse/baitapIfElse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace baitapIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, old;
+            int olds;
+            Console.Write("Nhap ten cua ban :");
+            name = Console.ReadLine();
+            Console.Write("Nhap tuoi cua ban :");
+            old = Console.ReadLine();
+            olds = Int32.Parse(old);
+            if (olds > 18)
+            {
+                Console.WriteLine("Xin chao " + name);
+            }
+            else
+            {
+                return;
+            }
+            Console.ReadKey();
+        }
+    }
+        
+}
