fe36c93c5a439aeb3061a642693266c2d7520f70
NguyenCuong <honguyencuong0898@gmail.com>
2020-03-22 11:13:23 +0700

    update

---
+++ Dates and times/baitapdateandtime/baitapdateandtime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace baitapdateandtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime ngaysinh;
+            string ngaysinhh;
+            
+            Console.Write("Nhap vao ngay sinh cua ban :");
+            ngaysinhh=Console.ReadLine();
+            ngaysinh = DateTime.Parse(ngaysinhh);
+            TimeSpan Daylife = DateTime.Today - ngaysinh;
+            Console.WriteLine("Ngay sinh cua ban la : " + ngaysinhh);
+            Console.WriteLine("So ngay ban da song : " + Daylife.TotalDays);
+            Console.ReadKey();
+            
+        }
+    }
+}
