92521b1988ae2aa684226a5c9705959a83a88584
NguyenCuong <honguyencuong0898@gmail.com>
2020-03-22 10:40:02 +0700

    dates and times

---
+++ Dates and times/Dates and times/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var currentTime = DateTime.Now; //Day and Time now
+        var today = DateTime.Today; // Day now
+        var someDate = new DateTime(2000, 7, 16); //Day have time to default 12am
+        var someMoment = new DateTime(2000, 7, 16, 8, 0, 0);  // Day have time 
+        var tomorrow = DateTime.Today.AddDays(1); // Next day
+        var yesterday = DateTime.Today.AddDays(-1); // Late day
+        var someDay = DateTime.Parse("16/7/2000"); // Date other
+        int year = currentTime.Year;
+        int month = currentTime.Month;
+        int second = currentTime.Second;
+        Console.WriteLine("Time now : " + currentTime);
+        Console.WriteLine("Day now : " + today);
+        Console.WriteLine("Some date : " + someDate);
+        Console.WriteLine("Some date have time : "+ someMoment);
+        Console.WriteLine("Tomorrow : "+ tomorrow);
+        Console.WriteLine("Yesterday : "+ yesterday);
+        Console.WriteLine("Some date other : "+ someDay);
+        Console.WriteLine("Year now : " + year);
+        Console.WriteLine("Month now : " + month);
+        Console.WriteLine("Second now: " + second);
+        Console.ReadKey();
+    }
+}
---
+++ Dates and times/tinhthoiluonggiuacacngay/tinhthoiluonggiuacacngay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        DateTime nextYear = new DateTime(DateTime.Today.Year + 1, 1, 1);
+        TimeSpan duration = nextYear - DateTime.Today;
+        Console.WriteLine("con lai {0} ngay de qua nam tiep theo!!",duration.TotalDays); //totalDays = chi lay ngay
+        Console.ReadKey();
+    }
+}
